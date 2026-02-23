16512b7c0cfcf7c7b56d978e41fb829e215b27f4
M K Mukesh Kannan <mukeshkannan311@gmail.com>
2026-02-23 11:25:11 +0530

    fix: realtime-projection while rendering

--- src/linear_regression/main.cpp
+++ src/linear_regression/main.cpp
@@ -17,49 +17,47 @@ int main(int, char **) {
 
 
   std::vector<double> epoch_list, error_list;
-  for (int _ = 0; _ < 100000; ++_) {
-    double error = 0;
-    for (int i = 0; i < ages.size(); ++i) error += pow((hypothesis(ages[i]) - prices[i]), 2);
-    error /= 2 * ages.size();
+  int epoch_number = 0;
 
-    double t1 = 0, t2 = 0;
-    for (int i = 0; i < ages.size(); ++i) {
-      t1 += (hypothesis(ages[i]) - prices[i]) * ages[i];
-      t2 += (hypothesis(ages[i]) - prices[i]);
-    };
-    t1 = t1 / ages.size();
-    t2 = t2 / ages.size();
-
-    O1 -= 0.001 * t1;
-    O2 -= 0.001 * t2;
-
-    epoch_list.push_back(_);
-    error_list.push_back(error);
-  };
-
-  double x[] = {0, -(O2 / O1)};
-  double y[] = {O2, 0};
   main_renderer.add_pipeline([&] {
+    epoch_number++;
+    if (epoch_number < 100000) {
+        double error = 0;
+        for (int i = 0; i < ages.size(); ++i) error += pow((hypothesis(ages[i]) - prices[i]), 2);
+        error /= 2 * ages.size();
+
+        double t1 = 0, t2 = 0;
+        for (int i = 0; i < ages.size(); ++i) {
+          t1 += (hypothesis(ages[i]) - prices[i]) * ages[i];
+          t2 += (hypothesis(ages[i]) - prices[i]);
+        };
+        t1 = t1 / ages.size();
+        t2 = t2 / ages.size();
+
+        O1 -= 0.001 * t1;
+        O2 -= 0.001 * t2;
+
+        epoch_list.push_back(epoch_number);
+        error_list.push_back(error);
+    };
+
+    double x[] = {0, -(O2 / O1)};
+    double y[] = {O2, 0};
+
+
     if (ImGui::Begin("DATA PLOT")) {
       if (ImPlot::BeginPlot("My Plot", ImVec2(600, 400))) {
-        ImPlot::PlotScatter("HOUSING PLOT", ages.data(), prices.data(),
-                            ages.size());
-
+        ImPlot::PlotScatter("HOUSING PLOT", ages.data(), prices.data(), ages.size());
         ImPlot::PlotLine("Line", x, y, 2);
-
         ImPlot::EndPlot();
       }
       ImGui::End();
     }
-  });
 
-  main_renderer.add_pipeline([&] {
     if (ImGui::Begin("LOSS FUNCTION")) {
       if (ImPlot::BeginPlot("LOSS", ImVec2(600, 400))) {
-
         ImPlot::PlotLine("LOSS", epoch_list.data(), error_list.data(), epoch_list.size());
         ImPlot::EndPlot();
-
       }
       ImGui::End();
     }
